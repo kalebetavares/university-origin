3a0189cfc3aa5e34d47a719cb9bbf4f7cc584973
Kalebe Tavares <kalebe.tavares@gmail.com>
2022-08-21 20:20:31 -0300

    fix:Verifica se contém cadeira

--- src/br/com/origin/university/system/test/TesteVerificaCadeira.java
+++ src/br/com/origin/university/system/test/TesteVerificaCadeira.java
@@ -11,9 +11,10 @@ public class TesteVerificaCadeira {
         Cadeira cadeira2 = new Cadeira("Algoritmo e Programação", "Carlos Porto", 88);
         Cadeira cadeira3 = new Cadeira("Introdução a Nutrição", "Fabiane Pinho", 66);
 
-
         curso1.adiciona(cadeira1);
         curso1.adiciona(cadeira2);
+
+        System.out.println(curso1);
 
         System.out.println(curso1.verificaCadeira(cadeira1));
         System.out.println(curso1.verificaCadeira(cadeira3));
